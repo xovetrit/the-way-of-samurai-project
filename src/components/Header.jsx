2766f13db089eb9b180e5bc35e7cf18315abbcce
Abylay <auzakbaev20@gmail.com>
2020-07-26 01:09:36 +0500

    first version

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src='https://www.freepngimg.com/thumb/symbol/95044-and-kovacs-dragon-circle-black-takeshi-white.png'></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Profile() {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://cdna.artstation.com/p/assets/images/images/005/514/686/large/gabriel-loo-carre-envdsn-cybpnkstreets-01.jpg?1491586828'></img>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default Profile;
