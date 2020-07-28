da93dcb21c18db3f529bcbd82eaea4d0c56ea76f
Abylay <auzakbaev20@gmail.com>
2020-07-28 11:32:41 +0500

    After Lesson 21 Messages Page

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,7 +4,30 @@ import classes from './Dialogs.module.css';
 function Dialogs(props){
     return(
         <div>
-            Dialogs
+            <div className={classes.dialogs}>
+                <div className={classes.dialogsItems}>
+                    <div className={classes.dialog + ' ' + classes.active}>
+                        Томас
+                    </div>
+                    <div className={classes.dialog}>
+                        Артур
+                    </div>
+                    <div className={classes.dialog}>
+                        Джон
+                    </div>
+                    <div className={classes.dialog}>
+                        Майкл
+                    </div>
+                    <div className={classes.dialog}>
+                        Финн
+                    </div>
+                </div>
+                <div className={classes.messages}>
+                    <div className={classes.message}>Hi!</div>
+                    <div className={classes.message}>How is your React the way of samurai?</div>
+                    <div className={classes.message}>Sir</div>
+                </div>
+            </div>
         </div>
     );
 }
