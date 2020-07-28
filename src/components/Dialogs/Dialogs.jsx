435226c806eedf5f6db5979c37e581640f33b58d
Abylay <auzakbaev20@gmail.com>
2020-07-28 17:14:01 +0500

    After Lesson 24/выносим данные в сторону BLL

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -18,21 +18,35 @@ function Message(props) {
 }
 
 function Dialogs(props) {
+
+    let messagesData = [
+        { message: 'Hi' },
+        { message: 'How is your React the way of samurai?' },
+        { message: 'Sir' }
+    ];
+
+    let dialogsData = [
+        { id: 1, name: 'Томас' },
+        { id: 2, name: 'Артур' },
+        { id: 3, name: 'Джон' },
+        { id: 4, name: 'Майкл' },
+        { id: 5, name: 'Финн' },
+    ];
+
     return (
         <div>
             <div className={classes.dialogs}>
                 <div className={classes.dialogsItems}>
-                    <DialogItem name='Томас' id='1' />
-                    <DialogItem name='Артур' id='2' />
-                    <DialogItem name='Джон' id='3' />
-                    <DialogItem name='Майкл' id='4' />
-                    <DialogItem name='Финн' id='5' />
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
+                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
+                    <DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
+                    <DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
                 </div>
                 <div className={classes.messages}>
-                    <Message message='Helloo' />
-                    <Message message='How is your React the way of samurai?' />
-                    <Message message='Sir' />
-                    <Message message='Sir' />
+                    <Message message={messagesData[0].message} />
+                    <Message message={messagesData[1].message} />
+                    <Message message={messagesData[2].message} />
                 </div>
             </div>
         </div>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -4,6 +4,12 @@ import Post from './Post/Post';
 
 function MyPosts() {
 
+    let postsData = [
+        { message: 'Если предали один раз - то это первый раз, если предали еще - то это уже второй раз', ava: 'https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' ,likesCount: 254},
+        { message: 'Работа не волк, работа это ворк', ava: 'https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' ,likesCount: 290},
+        { message: 'Лучше иметь друга, чем друг друга', ava: 'https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' ,likesCount: 364}
+    ];
+
     return (
         <div className={classes.MyPostsBlock}>
             <h3>My posts</h3>
@@ -16,9 +22,9 @@ function MyPosts() {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message='Если предали один раз - то это первый раз, если предали еще - то это уже второй раз' ava='https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' likesCount='254' />
-                <Post message='Работа не волк, работа это ворк' ava='https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' likesCount='290' />
-                <Post message='Лучше иметь друга, чем друг друга' ava='https://www.province.ru/pskov/media/k2/items/cache/080b4dffeeea98952b99d2915da0b69c_L.jpg' likesCount='364' />
+                <Post message={postsData[0].message} ava={postsData[0].ava} likesCount={postsData[0].likesCount} />
+                <Post message={postsData[1].message} ava={postsData[1].ava} likesCount={postsData[1].likesCount} />
+                <Post message={postsData[2].message} ava={postsData[2].ava} likesCount={postsData[2].likesCount} />
             </div>
         </div>
     );
