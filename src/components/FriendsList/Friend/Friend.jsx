cd0ffc9e4de7d365b24e6fda051eb2012f427826
Abylay <auzakbaev20@gmail.com>
2020-08-01 00:19:32 +0500

    FriendsList added styles

--- src/components/FriendsList/Friend/Friend.jsx
+++ src/components/FriendsList/Friend/Friend.jsx
@@ -4,11 +4,11 @@ import classes from './Friend.module.css';
 
 function Friend(props) {
     return (
-        <div>
-            <div>
+        <div className={classes.container}>
+            <div className={classes.FriendsListAll}>
                 <div className={classes.friendAvaPlace}><img className={classes.friendAvaImg} src={props.ava}></img></div>
-                {props.name}, {props.age}</div>
-            <div></div>
+                <div className={classes.friendNameAge}><p>{props.name}, {props.age} <span className={classes.OnlineStatus}>&bull;</span></p></div>
+            </div>
         </div>
     );
 }
